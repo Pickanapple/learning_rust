91780347f085a4f7dfd4dc1b8b363bc05552e032
pickanapple <thasselgreen@gmail.com>
2024-11-16 17:38:27 +0100

    removed target

---
+++ chapter_1/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\thass\OneDrive - Vestland fylkeskommune\Documents\privat\rust\the_book\chapter_1\hello_cargo\target\debug\deps\hello_cargo.exe: src/main.rs
+
+C:\Users\thass\OneDrive - Vestland fylkeskommune\Documents\privat\rust\the_book\chapter_1\hello_cargo\target\debug\deps\hello_cargo.d: src/main.rs
+
+src/main.rs:
