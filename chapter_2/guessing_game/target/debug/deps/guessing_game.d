47f658c8982f8238ee3b12e451a2e070dd9c755c
pickanapple <thasselgreen@gmail.com>
2024-11-16 17:32:38 +0100

    organising

--- chapter_2/guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thass\OneDrive - Vestland fylkeskommune\Documents\privat\rust\rust_the_book\chapter_2\guessing_game\target\debug\deps\guessing_game.exe: src/main.rs
-
-C:\Users\thass\OneDrive - Vestland fylkeskommune\Documents\privat\rust\rust_the_book\chapter_2\guessing_game\target\debug\deps\guessing_game.d: src/main.rs
-
-src/main.rs:
